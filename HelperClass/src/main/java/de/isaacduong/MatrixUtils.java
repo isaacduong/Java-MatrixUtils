e2126f8ccb1e682a4fc0a776f7dfeed228db2a3d
Isaac <55017345+isaacduong@users.noreply.github.com>
2020-08-25 16:41:25 +0200

    Initial commit

---
+++ HelperClass/src/main/java/de/isaacduong/MatrixUtils.java
@@ -0,0 +1,102 @@
+package de.isaacduong;
+
+public class MatrixUtils
+{
+
+	/**
+	 * @param matrixA 2-dimensional array
+	 * @param matrixB 2-dimensional array
+	 * @return sum of matrixA and matrixB
+	 * @throws Exception if matrixA and matrixB are of different shape
+	 */
+
+	public static int[][] matrixAdd(int[][] matrixA, int[][] matrixB) throws Exception
+	{
+
+		int[][] sum = new int[matrixA.length][matrixA[0].length];
+		if ((matrixA.length == matrixB.length) && (matrixA[0].length == matrixB[0].length))
+
+		{
+			for (int i = 0; i < matrixA.length; i++)
+			{
+				for (int j = 0; j < matrixA[0].length; j++)
+				{
+					sum[i][j] = matrixA[i][j] + matrixB[i][j];
+				}
+			}
+
+		}
+		else
+		{
+			throw new Exception("matrixes need to be of same shape");
+		}
+
+		return sum;
+
+	}
+
+	/**
+	 * @param matrixA 2-dimensional array
+	 * @param matrixB 2-dimensional array
+	 * @return difference of matrixA and matrixB
+	 * @throws Exception if matrixA and matrixB are of different shape
+	 */
+
+	public static int[][] matrixSub(int[][] matrixA, int[][] matrixB) throws Exception
+	{
+
+		int[][] dif = new int[matrixA.length][matrixA[0].length];
+		if ((matrixA.length == matrixB.length) && (matrixA[0].length == matrixB[0].length))
+
+		{
+			for (int i = 0; i < matrixA.length; i++)
+			{
+				for (int j = 0; j < matrixA[0].length; j++)
+				{
+					dif[i][j] = matrixA[i][j] - matrixB[i][j];
+				}
+			}
+
+		}
+		else
+		{
+			throw new Exception("matrixes need to be of same shape");
+		}
+
+		return dif;
+
+	}
+
+	/**
+	 * 
+	 * @param matrix to transpose
+	 * @return transposed Matrix
+	 * @throws Exception will be thrown if matrix is not of squared shape
+	 */
+	public static int[][] matrixTranspose(int[][] matrix) throws Exception
+	{
+
+		if (matrix.length == matrix[0].length)
+		{
+			int[][] matrixT = new int[matrix.length][matrix.length];
+			for (int i = 0; i < matrixT.length; i++)
+			{
+				for (int j = 0; j < matrixT.length; j++)
+				{
+					matrixT[i][j] = matrix[j][i];
+				}
+
+			}
+
+			return matrixT;
+
+		}
+		else
+		{
+			throw new Exception("it has to be a squared matrix ");
+
+		}
+
+	}
+
+}
---
+++ HelperClass/src/test/java/de/isaacduong/MatrixUtilsTest.java
@@ -0,0 +1,49 @@
+package de.isaacduong;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MatrixUtilsTest
+{
+	int[][] A = { { 1, 1 }, { 1, 1 } };
+	int[][] B = { { 2, 3 }, { 4, 5 } };
+	int[][] C = { { 2, 2 } };
+	int[][] D = { { 2, 2 }, { 2, 2 } };
+
+	@Test
+	void matrixAddTrueTest() throws Exception
+	{
+		assertArrayEquals(new int[][] { { 3, 4 }, { 5, 6 } }, MatrixUtils.matrixAdd(this.A, this.B));
+
+	}
+
+	@Test
+	void matrixAddFalseTest() throws Exception
+	{
+		assertNotEquals(new int[][] { { 1, 1 }, { 1, 1 } }, MatrixUtils.matrixAdd(this.A, this.D));
+
+	}
+
+	@Test
+	void matrixAddExpectedExeptionTest()
+	{
+		Assertions.assertThrows(Exception.class, () -> MatrixUtils.matrixAdd(this.A, this.C));
+	}
+
+	@Test
+	void matrixTransposeTest() throws Exception
+	{
+		assertArrayEquals(new int[][] { { 2, 4 }, { 3, 5 } }, MatrixUtils.matrixTranspose(this.B));
+
+	}
+
+	@Test
+	void matrixTransposeExpectedExceptionTest()
+	{
+		Assertions.assertThrows(Exception.class, () -> MatrixUtils.matrixTranspose(this.C));
+	}
+
+}
